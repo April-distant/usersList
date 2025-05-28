1babc01e6edc74a26104e06254666a8fc6cec57e
Alex <aprildistant@gmail.com>
2025-05-28 19:58:00 +0300

    Создали UI-компоненты: Card, Button

---
+++ client/src/components/Users/CreateUser.js
@@ -0,0 +1,25 @@
+import Button from '../UI/Button';
+import Card from '../UI/Card';
+import styles from './CreateUser.module.css'
+
+const CreateUser = () => {
+
+    return (
+        <Card className={styles.input}>
+            <form>
+                <label htmlFor="name">Имя</label>
+                <input id='name' type='text' />
+
+                <label htmlFor="age">Возраст</label>
+                <input id='age' type='number' />
+
+                {/* <button type='submit'>Добавить пользователя</button> */}
+                <Button type="submit">Добавить пользователя</Button>
+
+            </form>
+        </Card>
+    )
+
+}
+
+export default CreateUser;
