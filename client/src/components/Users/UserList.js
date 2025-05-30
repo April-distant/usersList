92a6da2af9edb8c0d0db39ccb50e9d1df07b665a
Alex <aprildistant@gmail.com>
2025-05-30 20:01:00 +0300

    Создали компонент UserList. Не до конца реализовали проброс данных из CreateUser в UserList.

---
+++ client/src/components/Users/UserList.js
@@ -0,0 +1,27 @@
+import Card from '../UI/Card';
+import styles from './UserList.module.css';
+
+const UserList = (props) => {
+    console.log(props)
+
+    // const numbers = [
+    //     { name: 'Alex', age: 15 },
+    //     { name: 'Max', age: 25 }
+    // ]
+
+    // const res = numbers.map((el) => `Я ${el.name}, мне ${el.age} лет`)
+
+    // console.log(res)
+
+    return (
+        <Card className={styles.users}>
+            <ul>
+                {props.users.map((user) => {
+                    <li hey={user.id}>{user.name} - {user.age} лет</li>
+                })}
+            </ul>
+        </Card>
+    )
+}
+
+export default UserList;
