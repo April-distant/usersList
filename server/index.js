e4db760fda889ee9ec9893676e6b548280583eea
Alex <aprildistant@gmail.com>
2025-06-03 20:01:21 +0300

    Начали развёртывать http-сервер.

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+console.log('Request', req)
+
+
+})
+
+server.listen(PORT, 'localhost', (error) => {
+    error 
+    ? console.log(`Ошибка! ${error}`) 
+    : console.log(`Listening PORT ${PORT}`)
+})
